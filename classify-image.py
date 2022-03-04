74976a7f3a0eb8f2a84747a2f26eb545f2c8fe6d
bkatona <30813752+bkatona@users.noreply.github.com>
2022-03-04 10:59:14 +0800

    Capture from webcam, button
    Add ability to capture an image from a webcam. Image classification runs when button pushed. Servos for the robot move.

--- classify-image.py
+++ classify-image.py
@@ -6,9 +6,42 @@ import cv2
 import os
 import sys, getopt
 import numpy as np
+import signal
 from edge_impulse_linux.image import ImageImpulseRunner
+from gpiozero import Button, Servo
 
 runner = None
+
+show_camera = False
+if (sys.platform == 'linux' and not os.environ.get('DISPLAY')):
+    show_camera = False
+
+def now():
+    return round(time.time() * 1000)
+
+def get_webcams():
+    port_ids = []
+    for port in range(5):
+        print("Looking for a camera in port %s:" %port)
+        camera = cv2.VideoCapture(port)
+        if camera.isOpened():
+            ret = camera.read()[0]
+            if ret:
+                backendName =camera.getBackendName()
+                w = camera.get(3)
+                h = camera.get(4)
+                print("Camera %s (%s x %s) found in port %s " %(backendName,h,w, port))
+                port_ids.append(port)
+            camera.release()
+    return port_ids
+
+def sigint_handler(sig, frame):
+    print('Interrupted')
+    if (runner):
+        runner.stop()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
 
 def help():
     print('python classify-image.py <path_to_model.eim> <path_to_image.jpg>')
@@ -35,41 +68,62 @@ def main(argv):
     modelfile = os.path.join(dir_path, model)
 
     print('MODEL: ' + modelfile)
-
+    
     with ImageImpulseRunner(modelfile) as runner:
         try:
             model_info = runner.init()
             print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
             labels = model_info['model_parameters']['labels']
 
-            img = cv2.imread(args[1])
-            if img is None:
-                print('Failed to load image', args[1])
-                exit(1)
+            if len(args)>= 2:
+                videoCaptureDeviceId = int(args[1])
+            else:
+                port_ids = get_webcams()
+                if len(port_ids) == 0:
+                    raise Exception('Cannot find any webcams')
+                if len(args)<= 1 and len(port_ids)> 1:
+                    raise Exception("Multiple cameras found. Add the camera port ID as a second argument to use to this script")
+                videoCaptureDeviceId = int(port_ids[0])
 
-            # imread returns images in BGR format, so we need to convert to RGB
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            camera = cv2.VideoCapture(videoCaptureDeviceId)
+            ret = camera.read()[0]
+            if ret:
+                backendName = camera.getBackendName()
+                w = camera.get(3)
+                h = camera.get(4)
+                print("Camera %s (%s x %s) in port %s selected." %(backendName,h,w, videoCaptureDeviceId))
+                camera.release()
+            else:
+                raise Exception("Couldn't initialize selected camera.")
+            
+            while True:
+                button.wait_for_press()
+                ret, img = capture.read()
+ 
 
-            # get_features_from_image also takes a crop direction arguments in case you don't have square images
-            features, cropped = runner.get_features_from_image(img)
+                # imread returns images in BGR format, so we need to convert to RGB
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            # the image will be resized and cropped, save a copy of the picture here
-            # so you can see what's being passed into the classifier
-            cv2.imwrite('debug.jpg', cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR))
+                # get_features_from_image also takes a crop direction arguments in case you don't have square images
+                features, cropped = runner.get_features_from_image(img)
 
-            res = runner.classify(features)
+                # the image will be resized and cropped, save a copy of the picture here
+                # so you can see what's being passed into the classifier
+                #cv2.imwrite('debug.jpg', cv2.cvtColor(cropped, cv2.COLOR_RGB2BGR))
 
-            if "classification" in res["result"].keys():
-                print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
-                for label in labels:
-                    score = res['result']['classification'][label]
-                    print('%s: %.2f\t' % (label, score), end='')
-                print('', flush=True)
+                res = runner.classify(features)
 
-            elif "bounding_boxes" in res["result"].keys():
-                print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
-                for bb in res["result"]["bounding_boxes"]:
-                    print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
+                if "classification" in res["result"].keys():
+                    print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
+                    for label in labels:
+                        score = res['result']['classification'][label]
+                        print('%s: %.2f\t' % (label, score), end='')
+                    print('', flush=True)
+
+                elif "bounding_boxes" in res["result"].keys():
+                    print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
+                    for bb in res["result"]["bounding_boxes"]:
+                        print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
         finally:
             if (runner):
                 runner.stop()
